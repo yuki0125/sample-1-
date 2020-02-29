dea9a2f25244161c5661141f9930788111b35d3a
yuki <>
2020-02-29 23:33:09 +0900

    no message

--- js/slick.js
+++ js/slick.js
@@ -1,3 +1,13 @@
+
+$(function() {
+  // 「#hide-text」要素に対するclickイベントを作成してください
+  $('qa').click(function() {
+    $('i')transform: rotate(180deg);
+  });
+
+});
+
+
 $(function(){
   $('.single-item').slick({
     accessibility: true,
